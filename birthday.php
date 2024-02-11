982752c3e1b2f217f59fa85c1154c66bb83918d1
MaruthiPrasadBallari <159635355+MaruthiPrasadBallari@users.noreply.github.com>
2024-02-11 10:22:51 +0530

    Add files via upload

---
+++ birthday.php
@@ -0,0 +1,11 @@
+
+<html>
+<head>
+
+</head>
+<body style="background: radial-gradient(blue,green,yellow,yellow);display: flex; align-items: center;justify-content: center;margin-top: 50px; background-size:cover; height:100vh ">
+    <div class="card" style="width: 700px; height: 600px; background-image:url(flat-golden-circle-balloons-birthday-background_52683-34659.webp);background-size: cover; border-radius: 10px;">
+        <h1 style="margin: 380px 100px 500px 350px; font-family: Georgia, 'Times New Roman', Times, serif; font-size: 50px; font-weight:bolder;color: rgba(255, 217, 0, 0.932);"><?php echo $_POST["name"]; ?></h1>
+    </div>
+</body>
+</html>
